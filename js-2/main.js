5c137b6ff5abc6a259ef55f6eb50bd0a0d8d8b9c
MD Hridoy Khan <mdhridoy31121997@gmail.com>
2025-04-07 23:48:07 +0600

    js-2

---
+++ js-2/main.js
@@ -0,0 +1,31 @@
+// // --------Artimetic Operators
+// let a = 5;
+// let b = 2;
+// console.log("a =",a,"& b = ",b);
+// console.log("a + b =",a + b);// joog
+// console.log("a - b =",a - b);// biyoog
+// console.log("a * b =",a * b);// gun
+// console.log("a / b =",a / b);// vaag
+// console.log("a % b =",a % b);// persenttise
+// console.log("a ** b =",a ** b);// too the power
+
+
+//Unary Operator
+let a = 5; //------Post Increment 
+let b = 2;
+
+console.log("a = ", " & b = ",b);
+// a = a +1;//6 2 ta niom ye kora jai
+a++;//6
+console.log("a = ",a);//6
+
+
+let x = 5;// ----- Post Decrement 
+let y = 2;
+
+console.log("x = ", " & y = ",y);
+// a = a -1;//4 2 ta niom ye kora jai
+x--;//4
+console.log("x = ",x);//4
+
+
