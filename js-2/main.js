31f3b4d2bf8762bd97cdd4d7023968fdc118645e
MD Hridoy Khan <mdhridoy31121997@gmail.com>
2025-04-13 19:27:43 +0600

    Logical Oprator

--- js-2/main.js
+++ js-2/main.js
@@ -1,61 +1,87 @@
- // --------Artimetic Operators
-// let a = 5;
+//  // --------Artimetic Operators
+// // let a = 5;
+// // let b = 2;
+// // console.log("a =",a,"& b = ",b);
+// // console.log("a + b =",a + b);// joog
+// // console.log("a - b =",a - b);// biyoog
+// // console.log("a * b =",a * b);// gun
+// // console.log("a / b =",a / b);// vaag
+// // console.log("a % b =",a % b);// persenttise
+// // console.log("a ** b =",a ** b);// too the power
+
+
+// //Unary Operator
+// let a = 5; //------Post Increment 
 // let b = 2;
-// console.log("a =",a,"& b = ",b);
-// console.log("a + b =",a + b);// joog
-// console.log("a - b =",a - b);// biyoog
-// console.log("a * b =",a * b);// gun
-// console.log("a / b =",a / b);// vaag
-// console.log("a % b =",a % b);// persenttise
-// console.log("a ** b =",a ** b);// too the power
+
+// console.log("a = ", " & b = ",b);
+// // a = a +1;//6 2 ta niom ye kora jai
+// a++;//6
+// console.log("a = ",a);//6
 
 
-//Unary Operator
-let a = 5; //------Post Increment 
-let b = 2;
+// let x = 5;// ----- Post Decrement 
+// let y = 2;
 
-console.log("a = ", " & b = ",b);
-// a = a +1;//6 2 ta niom ye kora jai
-a++;//6
-console.log("a = ",a);//6
-
-
-let x = 5;// ----- Post Decrement 
-let y = 2;
-
-console.log("x = ", " & y = ",y);
-// a = a -1;//4 2 ta niom ye kora jai
-x--;//4
-console.log("x = ",x);//4
+// console.log("x = ", " & y = ",y);
+// // a = a -1;//4 2 ta niom ye kora jai
+// x--;//4
+// console.log("x = ",x);//4
 
 
 
-//--------------Assainment opretor
+// //--------------Assainment opretor
 
-let ab = 5;
-let bc = 2;
-ab +=4;// a= a +4
-console.log("ab = ", ab);//9
+// let ab = 5;
+// let bc = 2;
+// ab +=4;// a= a +4
+// console.log("ab = ", ab);//9
 
-// jog bioyog gun vag persentse sob ei niom ye kora jai
+// // jog bioyog gun vag persentse sob ei niom ye kora jai
 
 
 
 
-// ------------Comparison Operator
-/*
-> -------------65 > 65----false
+// // ------------Comparison Operator
+// /*
+// > -------------65 > 65----false
 
->--------------25 > 25------false
+// >--------------25 > 25------false
 
->=------------86 >= 86-----true
+// >=------------86 >= 86-----true
 
-<=-----------25 <= 25-----true
+// <=-----------25 <= 25-----true
 
-==----------- 5== 0 ----false
+// ==----------- 5== 0 ----false
 
-!=------------ 25 != 25-----true
- */
-console.log(65 > 65);
+// !=------------ 25 != 25-----true
+//  */
+// console.log(65 > 65);
 
+
+
+ // ------------Logical Operator
+//  let a = 10;
+//  let b = 8;
+//  // && Logical And Operator
+//  let c = (a>b)&&(b<a);// sob gula  valu soman hobe thokn true dekhabe
+//  console.log( c);
+
+
+
+// let a = 10;
+// let b = 8;
+// // -----Logical || or operator
+// let c =(a>b)||(b<a);// || or operator je kono ekta stto holei true
+// console.log(c);
+
+
+let a = 10;
+let b = 8;
+let c = !(a<b);
+console.log(c);// ! not er khetre sotto hole mittha hobe ,r mittha hole sotto hobe, 
+
+
+
+ 
 
